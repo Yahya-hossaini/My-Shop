24f48e16ce2d6abd33d0478925a5b0dd33d60003
Yahya Hossaini <135443317+Yahya-hossaini@users.noreply.github.com>
2024-11-09 10:03:52 +0430

    setting provider state management technique:
    creating products provider class for the product overview screen.
    adding listener to the products grid file for the products_grid file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/providers/products_provider.dart';
 import 'package:my_shop/screens/product_detail_screen.dart';
 import 'package:my_shop/screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        useMaterial3: true,
-        fontFamily: 'Lato',
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.purple,
-          secondary: Colors.deepOrange,
+    return ChangeNotifierProvider(
+      create: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          useMaterial3: true,
+          fontFamily: 'Lato',
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.purple,
+            secondary: Colors.deepOrange,
+          ),
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.purple,
+          )
         ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.purple,
-        )
+        home: ProductsOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
       ),
-      home: ProductsOverviewScreen(),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
     );
   }
 }
