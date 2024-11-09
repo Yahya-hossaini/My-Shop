508aa165af83bab8ce12a55b581d715bb84736c8
Yahya Hossaini <135443317+Yahya-hossaini@users.noreply.github.com>
2024-11-09 11:31:57 +0430

    extracting thr product id from the products_provider file for the title of the product.

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:my_shop/providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -10,10 +12,11 @@ class ProductDetailScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final productID =  ModalRoute.of(context)?.settings.arguments as String;
+    final productID = ModalRoute.of(context)?.settings.arguments as String;
+    final loadedProduct = Provider.of<ProductsProvider>(context, listen: false).findById(productID);
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
