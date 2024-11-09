271c744641e71baf71dbb62262f0e71a100757b6
Yahya Hossaini <135443317+Yahya-hossaini@users.noreply.github.com>
2024-11-09 13:06:57 +0430

    Using nested providers:
    setting a provider for every single products.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,37 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:my_shop/screens/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  const ProductItem({
-    super.key,
-    required this.id,
-    required this.title,
-    required this.imageUrl,
-  });
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+  //
+  // const ProductItem({
+  //   super.key,
+  //   required this.id,
+  //   required this.title,
+  //   required this.imageUrl,
+  // });
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         child: GestureDetector(
           onTap: () {
-            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: id,);
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: product.id,);
           },
           child: Image.network(
-            imageUrl,
+            product.imageUrl,
             fit: BoxFit.cover,
           ),
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
           leading: IconButton(
-            icon: Icon(Icons.favorite),
-            onPressed: () {},
+            icon: Icon(product.isFavorite ? Icons.favorite : Icons.favorite_border),
+            onPressed: () {
+              product.toggleFavoriteStatus();
+            },
             color: Theme.of(context).colorScheme.secondary,
           ),
           trailing: IconButton(
@@ -40,7 +46,7 @@ class ProductItem extends StatelessWidget {
             color: Theme.of(context).colorScheme.secondary,
           ),
           title: Text(
-            title,
+            product.title,
             textAlign: TextAlign.center,
           ),
         ),
